01676338e413227a5e7effc3bb9639d3e7f9a7ac
Jeeshan Havi <jeeshanhavi@proton.me>
2026-07-23 18:26:53 +0530

    Add files via upload

---
+++ DataBase/ChatSets.py
@@ -0,0 +1,41 @@
+A1 = ["Hello Sir",
+      "Hello Sir, "
+      "How Are You Doing?",
+      "Hello Sir! I Am All Booted Up",
+      "Jarvis Here! All Set Up",
+      "Startup Launch Executed, Jarvis Is Online"]
+
+A2 = ["Good Morning Sir",
+      "Good Morning Sir!",
+      " Did You Sleep Well Last Night",
+      "Very Good Morning Sir",
+      "Good Morning Sir, Are You Sleepy Right Now?",
+      "Seems Like You Already Woke Up Sir, Wishing For The Day",
+      "Good Morning Sir, Have My  Wishing, You Had Any Nightmares",
+      "Greetings For The, Did You Had Any Good Dreams?"
+      "Hello Sir, Did You Had Dreams Last Night You Slept",
+      "Good Morning Sir! How Did You Slept Last Night",
+      "I Hope You Had A Really Nice Sleep",
+      "Hi Sir, Greetings, I Guess If You Didn't Have Any Dreams Like You Usually Do While You Were Asleep"]
+
+A3 = ["How Have Your Day Been Far By Now",
+      "Good Afternoon Sir!",
+      "Good Afternoon Sir! Did You Had Your Lunch",
+      "Good Afternoon Sir! Is The Day Sunny?",
+      "Greetings SIr!",
+      "Good Afternoon Sir! Did Had Any Micro Sleeps By Far The Day As You Usually Have?"]
+
+A4 = ["Good Evening!", "Hello Sir! Good Evening!",
+      "Good Evening!, Are Tired By The Day?",
+      "Greetings, Good Evening! Sir",
+      "Very Good Evening Sir! I Just Wet My Pants Today",
+      "Good Evening Sir!, Would You Like Some Tea Or Coffee",
+      "Good Evening Sir",
+      "Good Evening Sir, How Was Your Day",
+      "Good Evening Sir, I Guess If You Wanna Have Some Snacks",
+      "I Hope You Have A Great Evening Sir",
+      "Wishing You A Wonderful Evening Sir"
+      "How Are You Doing This Evening Sir?",
+      "A Pleasant Evening To You Sir"]
+
+S1 = ["Would LIke Me To Play A Video Related To Your Search", "Should I Play A Video Related To Your Search"]
